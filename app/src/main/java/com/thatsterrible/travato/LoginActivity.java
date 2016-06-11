cf1a02ceb8733caed99806bd4454f1e1ab37225c
Ruxuan <ruxuan.li@gmail.com>
2016-06-11 16:43:44 -0400

    Error Fix pls Naomi

--- app/src/main/java/com/thatsterrible/travato/LoginActivity.java
+++ app/src/main/java/com/thatsterrible/travato/LoginActivity.java
@@ -1,7 +1,20 @@
 package com.thatsterrible.travato;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+
 /**
  * Created by Gene42 Intern on 2016-06-11.
  */
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
 }
