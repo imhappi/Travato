5ba34a2867d7d80ac7e59258e2ec7a9e0ee89b1a
Alexander Duong <a9duong@uwaterloo.ca>
2016-06-11 17:50:41 -0400

    Added mytours activity layout and mytours item layout.

---
+++ app/src/main/java/com/thatsterrible/travato/MyToursActivity.java
@@ -0,0 +1,15 @@
+package com.thatsterrible.travato;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pandominator on 11/06/16.
+ */
+public class MyToursActivity extends AppCompatActivity{
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
