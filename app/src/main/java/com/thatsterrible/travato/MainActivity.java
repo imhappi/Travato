b8aa3011947bd28e5d7052b439537e6d3df8174a
Ruxuan <ruxuan.li@gmail.com>
2016-06-12 00:49:25 -0400

    Redirects based on facebook logged

--- app/src/main/java/com/thatsterrible/travato/MainActivity.java
+++ app/src/main/java/com/thatsterrible/travato/MainActivity.java
@@ -4,13 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+
 public class MainActivity extends AppCompatActivity {
+
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+
+        if (isLoggedIn()) {
+            Intent intent = new Intent(this, MyToursActivity.class);
+            startActivity(intent);
+        } else {
+
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
